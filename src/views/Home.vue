c34cb835434a51d45d9887463ee5a82016576441
Mike <michael.obsessvr.com>
2020-02-09 16:21:15 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld secondMsg="Task: Host on Netlify" />
+    <div>This page is rendered by views, which holds components with props</div>
+    <div>In this example: msg is the prop within the HelloWorld component rendered by Home.vue view.</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
