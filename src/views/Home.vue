f7eccedfb228e6852597c75119a5bf018da88460
Mike <michael.obsessvr.com>
2020-02-09 16:58:45 -0500

    First Build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,15 @@
     <HelloWorld secondMsg="Task: Host on Netlify" />
     <div>This page is rendered by views, which holds components with props</div>
     <div>In this example: msg is the prop within the HelloWorld component rendered by Home.vue view.</div>
+    <div>{{ message }}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+
+const message = "LOOK";
 
 export default {
   name: "Home",
